501aa97a00937a784f7ca7ee9a1e3f962f21735f
HarbX <michaelharborg@gmail.com>
2022-06-03 20:08:05 +0200

    Update Baseline.py

--- Baseline.py
+++ Baseline.py
@@ -16,9 +16,6 @@ import GPyOpt
 from data.base import get_data
 data = get_data()
 
-
-
-
 # # print(data.shape)
 
 # attributeNames = list(data.columns)
@@ -35,66 +32,19 @@ y = y.to_numpy()
 X = data.drop('transactionLocal_VAT_beforeDiscount', axis = 1)
 X = X.to_numpy()
 
-X = X[:1000, :]
-y = y[:1000]
 
 
+# X = X[:10000, :]
+# y = y[:10000]
 
 
-#%% RANDON FOREST REGRESSOR
-
-K1 = 5
-
-RMSE = []
-tscv = TimeSeriesSplit(n_splits = K1)
-
-# for train_index, test_index in tscv.split(X,y):
-#     Xtrain = X[train_index]
-#     Xtest = X[test_index]
-#     ytrain = y[train_index]
-#     ytest = y[test_index]
-        
-#     RFR = RandomForestRegressor(n_estimators = 100, criterion = "squared_error", min_samples_split = 10, min_samples_leaf = 2)
-#     RFR.fit(Xtrain,ytrain)
-#     preds = RFR.predict(Xtest)
-    
-#     RMSE.append(np.sqrt(mean_squared_error(ytest, preds)))
-    
-#%%  XGBOOST
-
-for train_index, test_index in tscv.split(X,y):
-    X_train = X[train_index]
-    X_test = X[test_index]
-    y_train = y[train_index]
-    y_test = y[test_index]
-    
-    xgb_reg = xgb.XGBRegressor(n_estimators=1000, enable_categorical = True)
-    xgb_reg.fit(X_train, y_train, eval_set=[(X_train, y_train), (X_test, y_test)], early_stopping_rounds=50, verbose=False)
-    
-    
-
-#%%
-
-### Parameters for a gridsearch if we want that :) 
-
-# params = {"n_estimators": [50, 100, 150, 200, 300, 500],
-#           "criterion": ["squared_error", "poisson"],
-#           "min_samples_split": [2, 5, 10, 20, 30],
-#           "min_samples_leaf": [1, 2, 5],
-#           "max_depth": [10, 5, None]
-#           }
-
-# RFR = RandomForestRegressor(n_estimators = 100, criterion = "squared_error", min_samples_split = 10, min_samples_leaf = 2)
-# RFR.fit(Xtrain,ytrain)
-# preds = RFR.predict(Xtest)
-# RMSE = np.sqrt(mean_squared_error(ytest, preds))
 
 
 
 
 #%% XGBOOST BAYESIAN OPTIMIZATION
 
-n_estimators_xgb = tuple(np.arange(50,500,50, dtype= np.int))
+n_estimators_xgb = tuple(np.arange(50,1000,10, dtype= np.int))
 # print(n_estimators)
 max_depth_xgb = tuple(np.arange(10,110,10, dtype= np.int))
 gamma_xgb = tuple(np.arange(1,10,1))
@@ -135,11 +85,11 @@ def objective_xgb(x):
     
     model = xgb.XGBRegressor(n_estimators=int(param[0]), max_depth = int(param[1]), min_child_weight = int(param[2]), gamma = int(param[3]), alpha = int(param[4]))
 
-    model.fit(Xtrain, ytrain)    
+    model.fit(X_train, y_train)    
     
-    preds = model.predict(Xtest)
+    preds = model.predict(X_test)
     
-    RMSE = np.sqrt(mean_squared_error(ytest, preds))
+    RMSE = np.sqrt(mean_squared_error(y_test, preds))
     
     return RMSE
 
@@ -152,11 +102,127 @@ opt.acquisition.exploration_weight=0.5
 opt.run_optimization(max_iter = 15) 
 
 x_best = opt.X[np.argmin(opt.Y)]
-print("The best parameters obtained: n_estimators=" + str(x_best[0]) + ", max_depth=" + str(x_best[1]) + ", max_features=" + str(
-    x_best[2])  + ", criterion=" + str(
-    x_best[3]))
+print("The best parameters obtained: n_estimators=" + str(x_best[0]) + ", max_depth=" + str(x_best[1]) + ", min_child_weight=" + str(
+    x_best[2])  + ", gamma=" + str(x_best[3]), "alpha" + str(x_best[4]))
     
     
+
+#%% GET FINAL PREDICTIONS
+n_estimators = int(x_best[0])
+max_depth = int(x_best[1])
+min_child_weights = int(x_best[2])
+gamma = (x_best[3])
+alpha = (x_best[4])
+
+K1 = 1
+
+RMSE = []
+tscv = TimeSeriesSplit(n_splits = K1)
+
+for train_index, test_index in tscv.split(X, y):
+    X_train = X[train_index]
+    X_test = X[test_index]
+    y_train = y[train_index]
+    y_test = y[test_index]
+    
+    xgb_reg = xgb.XGBRegressor(n_estimators=n_estimators, max_depth=max_depth, min_child_weights=min_child_weights, gamma=gamma, alpha=alpha)
+    xgb_reg.fit(X_train, y_train, eval_set=[(X_train, y_train), (X_test, y_test)], early_stopping_rounds=50, verbose=False, eval_metric = "rmse")
+    
+    preds = xgb_reg.predict(X_test)
+    
+    RMSE.append(np.sqrt(mean_squared_error(y_test, preds)))
+#%%   USE HYPERPARAMETERS AND ESTIMATE RMSE    
+
+
+
+
+K1 = 5
+
+RMSE = []
+tscv = TimeSeriesSplit(n_splits = K1)
+
+
+n_estimators = int(x_best[0])
+max_depth = int(x_best[1])
+min_child_weights = int(x_best[2])
+gamma = (x_best[3])
+alpha = (x_best[4])
+
+
+for train_index, test_index in tscv.split(X, y):
+    X_train = X[train_index]
+    X_test = X[test_index]
+    y_train = y[train_index]
+    y_test = y[test_index]
+    
+    xgb_reg = xgb.XGBRegressor(n_estimators=n_estimators, max_depth=max_depth, min_child_weights=min_child_weights, gamma=gamma, alpha=alpha)
+    xgb_reg.fit(X_train, y_train, eval_set=[(X_train, y_train), (X_test, y_test)], early_stopping_rounds=50, verbose=False, eval_metric = "rmse")
+    
+    preds = xgb_reg.predict(X_test)
+    
+    RMSE.append(np.sqrt(mean_squared_error(y_test, preds)))
+
+print(np.mean(RMSE))
+#%% RANDON FOREST REGRESSOR
+
+K1 = 5
+
+RMSE = []
+tscv = TimeSeriesSplit(n_splits = K1)
+
+# for train_index, test_index in tscv.split(X,y):
+#     Xtrain = X[train_index]
+#     Xtest = X[test_index]
+#     ytrain = y[train_index]
+#     ytest = y[test_index]
+        
+#     RFR = RandomForestRegressor(n_estimators = 100, criterion = "squared_error", min_samples_split = 10, min_samples_leaf = 2)
+#     RFR.fit(Xtrain,ytrain)
+#     preds = RFR.predict(Xtest)
+    
+#     RMSE.append(np.sqrt(mean_squared_error(ytest, preds)))
+    
+#%%  XGBOOST
+
+RMSE = []
+
+for train_index, test_index in tscv.split(X,y):
+    X_train = X[train_index]
+    X_test = X[test_index]
+    y_train = y[train_index]
+    y_test = y[test_index]
+    
+    xgb_reg = xgb.XGBRegressor(n_estimators=1000)
+    xgb_reg.fit(X_train, y_train, eval_set=[(X_train, y_train), (X_test, y_test)], early_stopping_rounds=50, verbose=False)
+    
+    preds = xgb_reg.predict(X_test)
+    
+    RMSE.append(np.sqrt(mean_squared_error(y_test, preds)))
+
+print(np.mean(RMSE))
+
+#%%
+
+### Parameters for a gridsearch if we want that :) 
+
+# params = {"n_estimators": [50, 100, 150, 200, 300, 500],
+#           "criterion": ["squared_error", "poisson"],
+#           "min_samples_split": [2, 5, 10, 20, 30],
+#           "min_samples_leaf": [1, 2, 5],
+#           "max_depth": [10, 5, None]
+#           }
+
+# RFR = RandomForestRegressor(n_estimators = 100, criterion = "squared_error", min_samples_split = 10, min_samples_leaf = 2)
+# RFR.fit(Xtrain,ytrain)
+# preds = RFR.predict(Xtest)
+# RMSE = np.sqrt(mean_squared_error(ytest, preds))
+
+
+
+
+
+
+
 
 #%%
 
