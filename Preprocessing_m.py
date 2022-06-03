3b5eab8dced2bbdd2aef558ba880c8b30f731682
HarbX <michaelharborg@gmail.com>
2022-06-03 17:22:15 +0200

    .

--- Preprocessing_m.py
+++ Preprocessing_m.py
@@ -7,6 +7,11 @@ mc = pd.read_csv("data/master_calendar.csv")
 mpde = pd.read_csv("data/master_planday_shifts_encoded.csv")
 
 
+
+#%%
+print(mpde['starthour_rounded'][0])
+print(mpde["employeeID"][0])
+#%%
 mc_a_names = list(mc.columns)
 N = len(mc)
 for i in range(N):
@@ -15,22 +20,30 @@ for i in range(N):
     else:
         mc['Type'][i] = 1
         
-        
+#%%
 N = len(mpde)
 mpde_names = list(mpde.columns)
 
-# arr = mpde.to_numpy()
+arr = mpde.to_numpy()
 
-# print("shape before: ", arr.shape)
+print("shape before: ", arr.shape)
+
+dropped = []
 for i in range(N):
     for j in range(N):
-        if mpde['starthour_rounded'][i] == mpde['starthour_rounded'][j]:
-            if mpde["employeeID"][i] == mpde["employeeID"][j]:
-                mpde = mpde.drop(axis=0, index = i)
+        if not i in dropped and not j in dropped:
+            if mpde['starthour_rounded'][i] == mpde['starthour_rounded'][j]:
+                if mpde["employeeID"][i] == mpde["employeeID"][j]:
+                    dropped.append(i)
+            else:
+                break
+            
+mpde = mpde.drop(axis=0, index = dropped)
+
                 
-# arr2 = mpde.to_numpy()
-# print("shape_after: ", arr2.shape)
+                
+arr2 = mpde.to_numpy()
+print("shape_after: ", arr2.shape)
                 
 #%%
-
-print(mpde[0])
+print(mpde['starthour_rounded'][0])
